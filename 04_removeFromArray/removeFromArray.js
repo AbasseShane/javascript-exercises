a646bea494c71f0ca9e9049c0bad4d05634f8ffb
Shane Abasse <aabasse.shane@gmail.com>
2024-02-11 20:16:05 +0800

    added answer for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //should return [1,2,4] when given removeFromArray([1,2,3,4],3)
 
+    let newArray = []
+
+    array.forEach(item => {
+        
+        if(!args.includes(item))
+        {
+            newArray.push(item);
+        } 
+    });
+
+    return newArray
 };
 
 // Do not edit below this line
