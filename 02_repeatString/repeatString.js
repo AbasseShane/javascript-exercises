d53bff718603a660358fc0addc78318fc451ad13
Shane Abasse <aabasse.shane@gmail.com>
2024-02-11 18:30:35 +0800

    added exercice 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, number) {
+    //repeatString('hey', 3) will display  'heyheyhey'
 
+    if(number < 0)
+        return 'ERROR'
+    let text = ""
+    for(let i=0; i<number; i++)
+    {
+        text+=str
+    }
+    return text
 };
 
 // Do not edit below this line
